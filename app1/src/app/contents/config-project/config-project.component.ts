0ce31a000c469aea62f75f0cd0131c2cada2b48d
rnsytmsk <rnsytmsk@gmail.com>
2024-08-04 12:55:59 +0900

    スケルトンコード

---
+++ app1/src/app/contents/config-project/config-project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-config-project',
+  standalone: true,
+  imports: [],
+  templateUrl: './config-project.component.html',
+  styleUrl: './config-project.component.css'
+})
+export class ConfigProjectComponent {
+
+}
