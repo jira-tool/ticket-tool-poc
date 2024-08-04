3f67b8009e090c3c97e05a2920733eaf870474f6
rnsytmsk <rnsytmsk@gmail.com>
2024-08-04 13:54:39 +0900

    メニュー変更

--- app1/src/app/side-navigator/side-navigator.component.ts
+++ app1/src/app/side-navigator/side-navigator.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-side-navigator',
   standalone: true,
-  imports: [MatSidenavModule, MatSelectModule, MatListModule],
+  imports: [MatSidenavModule, MatSelectModule, MatListModule, MatToolbar],
   templateUrl: './side-navigator.component.html',
   styleUrl: './side-navigator.component.css'
 })
